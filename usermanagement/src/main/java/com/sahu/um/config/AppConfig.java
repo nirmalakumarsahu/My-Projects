c1957c624b3a7638f01374e449ef99d95a992901
Nirmala9955 <nirmalakumarsahu7@gmail.com>
2023-05-25 08:39:20 +0530

    Properties files

--- usermanagement/src/main/java/com/sahu/um/config/AppConfig.java
+++ usermanagement/src/main/java/com/sahu/um/config/AppConfig.java
@@ -9,11 +9,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @PropertySources({
 	@PropertySource("classpath:properties/login.properties"),
-	@PropertySource("classpath:properties/registration.properties"),
-	@PropertySource("classpath:properties/forgetPassword.properties"),
 	@PropertySource("classpath:properties/role.properties"),
 	@PropertySource("classpath:properties/permission.properties"),
-	@PropertySource("classpath:properties/error.properties")
+	@PropertySource("classpath:properties/error.properties"),
+	@PropertySource("classpath:properties/user.properties")
 })
 public class AppConfig {
 
