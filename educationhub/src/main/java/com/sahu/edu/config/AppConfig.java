1ef0a19313a90bae71996bd9595b8fb1c905267b
Nirmala9955 <nirmalakumarsahu7@gmail.com>
2023-06-25 18:47:26 +0530

    added

---
+++ educationhub/src/main/java/com/sahu/edu/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.sahu.edu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@PropertySources({
+	@PropertySource("classpath:properties/error.properties"),
+	@PropertySource("classpath:properties/header_footer.properties"),
+	@PropertySource("classpath:properties/login.properties"),
+	@PropertySource("classpath:properties/role.properties"),
+	@PropertySource("classpath:properties/user.properties")
+})
+public class AppConfig {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
---
+++ educationhub/src/main/java/com/sahu/edu/config/SecurityConfiguration.java
@@ -0,0 +1,50 @@
+package com.sahu.edu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+	
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+	    return (web) -> web.ignoring().antMatchers("/css/**", "/js/**", "/images/**", "/lib/**");
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(request->
+				request.antMatchers("/", "/login", "/registration", "/forgetpassword").permitAll()
+				.anyRequest().authenticated()
+			)
+			.csrf().disable()
+			.formLogin(form -> form
+					.loginPage("/login")
+					.loginProcessingUrl("/login")
+					.failureUrl("/login?error")
+					.usernameParameter("username")
+					.passwordParameter("password")
+					.defaultSuccessUrl("/client/user/dashboard", true))
+			.logout(logout -> logout 
+					.logoutUrl("/logout")
+					.invalidateHttpSession(true)
+					.deleteCookies("JSESSIONID")
+					.logoutSuccessUrl("/login?logout"))
+			.sessionManagement(session -> session
+					.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+					.invalidSessionUrl("/invalidsession")
+					.maximumSessions(1)
+					.maxSessionsPreventsLogin(false)
+					.expiredUrl("/login?session-expire"))
+			.exceptionHandling()
+			.accessDeniedPage("/accessdenied");
+		return http.build();
+	}
+	
+}
